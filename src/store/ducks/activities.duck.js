57f4833037734a291ce3c8a7c975f90e9da7b65a
Matias Benary <matiasbenary@gmai.com>
2020-03-11 12:37:28 -0300

    :beers: Se agrego varible de entorno para filtrar actividades por id

--- src/store/ducks/activities.duck.js
+++ src/store/ducks/activities.duck.js
@@ -92,7 +92,7 @@ export const actions = {
 
 export function* getActivitiesState() {
   try {
-    const results = yield call(apiCall, 'activity', null, 'GET');
+    const results = yield call(apiCall, `activity/?filter[entity_origin_id]=${process.env.REACT_APP_ID_ENTITY}`, null, 'GET');
     yield put({ type: actionTypes.GetActivitiesComplete, results });
   } catch (error) {
     yield put({ type: actionTypes.GetActivitiesError, error });
