9c52f6cca7f8107dcef1dc6ee096d5dabdd1d3ea
Matias Benary <matiasbenary@gmail.com>
2020-05-20 11:15:13 -0300

    :robot: se cambio configuracion de Ada

--- src/config/3221/3221.js
+++ src/config/3221/3221.js
@@ -1,8 +1,8 @@
-import Logo from './logo.png';
-import Icon from './icon.png';
+import Logo from "./logo.png";
+import Icon from "./icon.png";
 
 export default {
   logo: Logo,
   icon: Icon,
-  title: 'Voluntariado Ada',
+  title: "Actividades de voluntariado"
 };
