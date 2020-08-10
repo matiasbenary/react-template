874ff5c740d2b08200a49676475034d9b79ec848
Matias Benary <matiasbenary@gmail.com>
2020-08-10 15:57:28 -0300

    :bug: Fix routes login

--- src/components/main/Auth.jsx
+++ src/components/main/Auth.jsx
@@ -7,6 +7,7 @@ import config from "../../config";
 import ResetUser from "../pages/ResetUser";
 import Login2 from "../pages/Login2";
 import Register from "../pages/Register";
+import ResetPass from "../pages/ResetPass";
 
 const Img = styled.img`
   width: ${props => props.width || "100%"};
@@ -14,7 +15,7 @@ const Img = styled.img`
   display: block;
 `;
 
-const Main = ({ children }) => (
+const Main = () => (
   <div className="body">
     <div className="login">
       <div className="login__title">
@@ -25,9 +26,9 @@ const Main = ({ children }) => (
       <Switch>
         <Route exact path="/reset" component={ResetUser} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/password/reset/:id" component={ResetPass} />
         <Route path="/" component={Login2} />
       </Switch>
-      {children}
     </div>
   </div>
 );
--- src/components/pages/ResetPass/index.jsx
+++ src/components/pages/ResetPass/index.jsx
@@ -1,42 +1,44 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { createSelector } from 'reselect';
-import Loader from 'react-spinners/PropagateLoader';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import { actions } from '../../../store/ducks/auth.duck';
-import WarningSpan from '../../molecules/WarningSpan';
-import SuccessSpan from '../../molecules/SuccessSpan';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { createSelector } from "reselect";
+import Loader from "react-spinners/PropagateLoader";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { actions } from "../../../store/ducks/auth.duck";
+import WarningSpan from "../../molecules/WarningSpan";
+import SuccessSpan from "../../molecules/SuccessSpan";
 
 const Schema = Yup.object({
   email: Yup.string()
-    .email('Email Invalido')
-    .required('Requerido'),
-  password: Yup.string().required('Requerido').min(8, 'Debe contener al menos 8 caracteres.'),
-  password_confirmation: Yup.string().required('Requerido').when('password', {
-    is: (val) => (!!(val && val.length > 0)),
-    then: Yup.string().oneOf(
-      [Yup.ref('password')],
-      'Debe coincidir con la contraseña',
-    ),
-  }),
+    .email("Email Invalido")
+    .required("Requerido"),
+  password: Yup.string()
+    .required("Requerido")
+    .min(8, "Debe contener al menos 8 caracteres."),
+  password_confirmation: Yup.string()
+    .required("Requerido")
+    .when("password", {
+      is: val => !!(val && val.length > 0),
+      then: Yup.string().oneOf(
+        [Yup.ref("password")],
+        "Debe coincidir con la contraseña"
+      )
+    })
 });
 
+const loadingReset = state => state.auth.loadingReset;
+const msjReset = state => state.auth.msjReset;
+const errorReset = state => state.auth.errorReset;
 
-const loadingReset = (state) => state.auth.loadingReset;
-const msjReset = (state) => state.auth.msjReset;
-const errorReset = (state) => state.auth.errorReset;
+const loadingSelector = () => createSelector(loadingReset, loading => loading);
 
-const loadingSelector = () => createSelector(loadingReset, (loading) => loading);
+const msjSelector = () => createSelector(msjReset, msj => msj);
 
-const msjSelector = () => createSelector(msjReset, (msj) => msj);
+const errorSelector = () => createSelector(errorReset, error => error);
 
-const errorSelector = () => createSelector(errorReset, (error) => error);
-
-const Reset = () => {
+const ResetPass = () => {
   const { id } = useParams();
   const loading = useSelector(loadingSelector());
   const msj = useSelector(msjSelector());
@@ -46,37 +48,37 @@ const Reset = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: '',
-      password: '',
-      password_confirmation: '',
+      email: "",
+      password: "",
+      password_confirmation: ""
     },
     validationSchema: Schema,
-    onSubmit: (values) => {
+    onSubmit: values => {
       const payload = { ...values, token: id };
       dispatch(actions.reset(payload));
-    },
+    }
   });
 
   useEffect(() => {
     if (error) {
       formik.setErrors({
         ...formik.errors,
-        message: 'Email o url invalido',
+        message: "Email o url invalido"
       });
     }
   }, [error]);
 
-
   return (
     <form onSubmit={formik.handleSubmit}>
+      <span className="login__title_text">Restablecer Contraseña</span>
       <div className="login__inputs">
-      {formik.errors.message ? (
-            <WarningSpan msj={formik.errors.message} />
-          ) : null}
+        {formik.errors.message ? (
+          <WarningSpan msj={formik.errors.message} />
+        ) : null}
 
-          { (!error && msj)
-          ? <SuccessSpan msj="¡Se ha restablecido tu contraseña con exito!" />
-          : null}
+        {!error && msj ? (
+          <SuccessSpan msj="¡Se ha restablecido tu contraseña con exito!" />
+        ) : null}
         <div className="login__input_group">
           <label htmlFor="username" className="login__label">
             Correo electrónico
@@ -86,7 +88,7 @@ const Reset = () => {
             type="email"
             name="email"
             className="login__input"
-            {...formik.getFieldProps('email')}
+            {...formik.getFieldProps("email")}
           />
           {formik.touched.email && formik.errors.email ? (
             <WarningSpan msj={formik.errors.email} />
@@ -101,7 +103,7 @@ const Reset = () => {
             type="password"
             name="password"
             className="login__input"
-            {...formik.getFieldProps('password')}
+            {...formik.getFieldProps("password")}
             autoComplete="new-password"
           />
           {formik.touched.password && formik.errors.password ? (
@@ -110,17 +112,18 @@ const Reset = () => {
         </div>
         <div className="login__input_group">
           <label htmlFor="password_confirmation" className="login__label">
-            <span>Confirmar  Contraseña </span>
+            <span>Confirmar Contraseña </span>
           </label>
           <input
             id="password_confirmation"
             type="password"
             name="password_confirmation"
             className="login__input"
-            {...formik.getFieldProps('password_confirmation')}
+            {...formik.getFieldProps("password_confirmation")}
             autoComplete="new-password"
           />
-          {formik.touched.password_confirmation && formik.errors.password_confirmation ? (
+          {formik.touched.password_confirmation &&
+          formik.errors.password_confirmation ? (
             <WarningSpan msj={formik.errors.password_confirmation} />
           ) : null}
         </div>
@@ -128,21 +131,19 @@ const Reset = () => {
       <div className="login__buttons">
         {loading ? (
           <div
-    style={{
-      height: '38px',
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
+            style={{
+              height: "38px",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
           >
-    <Loader size={15} color="#007bff" loading />
+            <Loader size={15} color="#007bff" loading />
           </div>
-        )
-
-        : !error && msj ? (
+        ) : !error && msj ? (
           <Link className="btn btn-primary btn__login" to="/">
-              Ir a iniciar Sesión
+            Ir a iniciar Sesión
           </Link>
         ) : (
           <button className="btn btn-primary btn__login" type="submit">
@@ -154,6 +155,4 @@ const Reset = () => {
   );
 };
 
-Reset.propTypes = {};
-
-export default Reset;
+export default ResetPass;
